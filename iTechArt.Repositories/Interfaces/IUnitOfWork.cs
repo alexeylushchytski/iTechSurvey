85dd65fcc6f274ccbb93566eac9a2ad9cdde08ff
Alexey Lushchytski <aliaksei.lushchytski@itechart-group.com>
2018-03-07 15:32:34 +0300

    Added new layers for DAL

---
+++ iTechArt.Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+
+
+        void Dispose();
+
+
+        IRepository<T> GetRepository<T>() where T : class;
+    }
+}

8dcd77a3e75dff7add7b138c5d4c73ae259d4257
Alexey Lushchytski <aliaksei.lushchytski@itechart-group.com>
2018-03-09 13:21:27 +0300

    New folders for versioning

---
+++ Controllers/Controllers/V1/UserController.cs
@@ -0,0 +1,41 @@
+﻿using iTechArt.Survey.BLL.Interfaces;
+using Microsoft.Web.Http;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace iTechArt.Survey.WebApi.Controllers.V1
+{
+    [ApiVersion("1")]
+    [RoutePrefix("api/v{version:ApiVersion}/User")]
+    public class UserController : ApiController
+    {
+        private readonly IUserService _userService;
+
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpGet]
+        [Route("Users")]
+        public HttpResponseMessage Users()
+        {
+            try
+            {
+                var response = Request.CreateResponse(HttpStatusCode.OK);
+                if (response != null)
+                {
+                    return response;
+                }
+                throw new HttpResponseException(HttpStatusCode.NoContent);
+            }
+            catch (HttpResponseException exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NoContent, exception);
+            }
+        }
+    }
+}
---
+++ Controllers/Controllers/V2/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using iTechArt.Survey.BLL.Interfaces;
+using Microsoft.Web.Http;
+
+namespace iTechArt.Survey.WebApi.Controllers.V2
+{
+    [ApiVersion("2")]
+    [RoutePrefix("api/v{version:ApiVersion}/User")]
+    public class UserController : ApiController
+    {
+        private readonly IUserService _userService;
+
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpGet]
+        [Route("Users")]
+        public HttpResponseMessage Users()
+        {
+            try
+            {
+                var response = Request.CreateResponse(HttpStatusCode.OK, _userService.GetUsers());
+                if (response != null)
+                {
+                    return response;
+                }
+                throw new HttpResponseException(HttpStatusCode.NoContent);
+            }
+            catch (HttpResponseException exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NoContent, exception);
+            }
+        }
+    }
+}

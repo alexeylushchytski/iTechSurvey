280515b00b2b269604be67ec93a7e818b6528b12
Alexey Lushchytski <aliaksei.lushchytski@itechart-group.com>
2018-03-13 12:05:49 +0300

    Initial migration

---
+++ DAL/Migrations/201803130904498_InitialMigration.cs
@@ -0,0 +1,28 @@
+namespace iTechart.Survey.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        Email = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}

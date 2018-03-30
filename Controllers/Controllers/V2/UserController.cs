a95ec52ce134d70093e96af7ec9e6d349283d013
Alexey Lushchytski <aliaksei.lushchytski@itechart-group.com>
2018-03-30 17:03:54 +0300

    pagination done

--- Controllers/Controllers/V2/UserController.cs
+++ Controllers/Controllers/V2/UserController.cs
@@ -1,8 +1,12 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using iTechArt.Common.Logger.LoggerContext;
+using iTechArt.Survey.BLL.DTO.ViewModels;
 using iTechArt.Survey.BLL.Interfaces;
 using iTechArt.Survey.WebApi.Attributes;
 using Microsoft.Web.Http;
@@ -25,12 +29,22 @@ namespace iTechArt.Survey.WebApi.Controllers.V2
         [HttpGet]
         [Route("Users")]
         [ClaimsAuthorization(ClaimType = "Role", ClaimValue = "Admin")]
-        public async Task<HttpResponseMessage> Users()
+        public async Task<HttpResponseMessage> Users(int page = 1)
         {
             try
             {
                 LoggerContext.Current.Log(Request.ToString());
-                var response = Request.CreateResponse(HttpStatusCode.OK, await _userService.GetUserViewModels());
+                List<UserViewModel> users = (List<UserViewModel>)await _userService.GetUserViewModels();
+                int limit = 2 >= users.Count ? users.Count : 2;
+                int offset = (page - 1) * limit;
+                var resultUsers = (from user in users select user).AsQueryable().Skip(offset).Take(limit).ToList();
+                var responseData = new
+                {
+                    resultUsers,
+                    page,
+                    totalPages = (int)Math.Ceiling(users.Count / (double)limit)
+            };
+                var response = Request.CreateResponse(HttpStatusCode.OK, responseData);
                 if (response != null)
                 {
                     LoggerContext.Current.Log(response.ToString());
